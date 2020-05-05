a66b242cd2c48f26f78c3bf0731ee706e95a7e9f
lpwang <13889522960@163.com>
2020-05-05 22:46:07 +0800

    调整代码结构，删除多余代码，重新设计routers

---
+++ routers/generator_router.go
@@ -0,0 +1,25 @@
+package routers
+
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"github.com/kdexer/distributed-uid-generator/generator"
+	"net/http"
+)
+
+type GeneratorRouter struct {
+	generator generator.NextId
+}
+
+func NewGeneratorRouter(generator generator.NextId) *GeneratorRouter {
+	return &GeneratorRouter{generator: generator}
+}
+
+func (gr GeneratorRouter) GetHandlerFunction() httprouter.Handle {
+	handler := func (rw http.ResponseWriter,re *http.Request, pa httprouter.Params) {
+		uid := gr.generator.GetNextId()
+		fmt.Println(uid)
+		fmt.Fprint(rw, uid)
+	}
+	return handler
+}
